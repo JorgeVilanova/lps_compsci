4e4a05f68a5b0b7c4c31988cdf4241048a57ce17
Jorge Vilanova <rc.vilanova.jorge@leadps.org>
2017-03-27 16:59:13 +0000

    This gives a random word, combined by listlength, randomnumber, and wordreader

---
+++ class_samples/5-6_wordslists/randomWord.py
@@ -0,0 +1,24 @@
+#create a random word from words.txt
+import random
+
+wordsFile = open("words.txt", "r")
+
+wordsList = []
+myWord = wordsFile.readline()
+while myWord != '':
+        #as long as there are more words,
+        #put the word in the list and read another
+
+        wordsList.append(myWord)
+	myWord = wordsFile.readline()
+
+listLength = len(wordsList)
+
+randint = random.randint(0, listLength)
+randWord = wordsList[randint]
+print(randWord)
+
+
+
+
+
